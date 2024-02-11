0bf7473c5290e0c76fe02cf78ca9557a1a494abe
Ganesh Pawar <gapawar2003@gmail.com>
2024-02-11 08:50:14 +0530

    1

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,6 +7,7 @@ export const verifyAccessToken = asyncHandler(async (req, res, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
+    console.log(token);
 
     if (!token) {
       throw new ApiError(402, "Unathorized request");
